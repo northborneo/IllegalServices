9bd3b6e62b154f526a928c40759d219c2915e5e9
Illegal-Services <BUZZARDGTA@protonmail.com>
2022-02-27 15:20:13 +0100

    v6.1.0.7 - 27/02/2022

--- Illegal_Services.bat
+++ Illegal_Services.bat
@@ -8,8 +8,8 @@ REM  Copyrights: Copyright (C) 2020 IB_U_Z_Z_A_R_Dl
 REM  Trademarks: Copyright (C) 2020 IB_U_Z_Z_A_R_Dl
 REM  Originalname: Illegal_Services.exe
 REM  Comments: Illegal Services
-REM  Productversion:  6. 1. 0. 5
-REM  Fileversion:  6. 1. 0. 5
+REM  Productversion:  6. 1. 0. 7
+REM  Fileversion:  6. 1. 0. 7
 REM  Internalname: Illegal_Services.exe
 REM  Appicon: Ressources\Icons\icon.ico
 REM  AdministratorManifest: Yes
@@ -157,7 +157,7 @@ popd
 :LAUNCHER
 if defined VERSION set OLD_VERSION=!VERSION!
 if defined lastversion set OLD_LASTVERSION=!lastversion!
-set VERSION=v6.1.0.6 - 26/02/2022
+set VERSION=v6.1.0.7 - 27/02/2022
 set "el=UNDERLINE=!\E![04m,UNDERLINEOFF=!\E![24m,BLACK=!\E![30m,RED=!\E![31m,GREEN=!\E![32m,YELLOW=!\E![33m,BLUE=!\E![34m,MAGENTA=!\E![35m,CYAN=!\E![36m,WHITE=!\E![37m,BGBLACK=!\E![40m,BGYELLOW=!\E![43m,BGWHITE=!\E![47m,BGBRIGHTBLACK=!\E![100m,BRIGHTBLACK=!\E![90m,BRIGHTRED=!\E![91m,BRIGHTBLUE=!\E![94m,BRIGHTMAGENTA=!\E![95m"
 set "%el:,=" && set "%"
 echo !BGBLACK!!BRIGHTBLUE!
@@ -3153,7 +3153,6 @@ for %%A in (0 1) do if "!YouTubeDLGeoBypass!"=="%%A" exit /b
 call :ERROR_REGEDIT YouTubeDLGeoBypass YouTubeDLGeoBypass
 >nul reg add "!IS_REG!" /v "YouTubeDLGeoBypass" /t REG_DWORD /d 0 /f
 goto :CHECK_YOUTUBEDLGEOBYPASS
-
 
 :CHECK_YOUTUBEDLPRIORITY
 set YouTubeDLPriority=
