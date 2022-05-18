3551cfd756f7fb3ead17e2fb42768670831833b2
Illegal-Services <BUZZARDGTA@protonmail.com>
2022-05-18 13:36:40 +0200

    v6.1.5.4 - 18/05/2022

--- Illegal_Services.bat
+++ Illegal_Services.bat
@@ -8,8 +8,8 @@ REM  Copyrights: Copyright (C) 2022 IB_U_Z_Z_A_R_Dl
 REM  Trademarks: Copyright (C) 2022 IB_U_Z_Z_A_R_Dl
 REM  Originalname: Illegal_Services.exe
 REM  Comments: Illegal Services
-REM  Productversion:  6. 1. 5. 3
-REM  Fileversion:  6. 1. 5. 3
+REM  Productversion:  6. 1. 5. 4
+REM  Fileversion:  6. 1. 5. 4
 REM  Internalname: Illegal_Services.exe
 REM  Appicon: Ressources\Icons\icon.ico
 REM  AdministratorManifest: Yes
@@ -28,9 +28,11 @@ if not exist "%IS_PATH_BAT_USED%" (
         <"%IS_PATH_BAT_USED%" find /v ""
     )
     (
-        >nul move /y "[CRLF]_Illegal_Services.bat" "%IS_PATH_BAT_USED%"
-        call "%IS_PATH_BAT_USED%"
-        exit 0
+        >nul move /y "[CRLF]_Illegal_Services.bat" "%IS_PATH_BAT_USED%" && (
+            call "%IS_PATH_BAT_USED%" && (
+                exit 0
+            )
+        )
     )
 )
 >nul findstr /v "$" "%IS_PATH_BAT_USED%" && (
@@ -194,7 +196,7 @@ for /f %%A in ('2^>nul dir "!TMPF!\????????.bat" /a:-d /o:-d /b ^| findstr /rxc:
 :LAUNCHER
 if defined VERSION set OLD_VERSION=!VERSION!
 if defined lastversion set OLD_LASTVERSION=!lastversion!
-set VERSION=v6.1.5.3 - 18/05/2022
+set VERSION=v6.1.5.4 - 18/05/2022
 set "el=UNDERLINE=!\E![04m,UNDERLINEOFF=!\E![24m,BLACK=!\E![30m,RED=!\E![31m,GREEN=!\E![32m,YELLOW=!\E![33m,BLUE=!\E![34m,MAGENTA=!\E![35m,CYAN=!\E![36m,WHITE=!\E![37m,BGBLACK=!\E![40m,BGYELLOW=!\E![43m,BGWHITE=!\E![47m,BGBRIGHTBLACK=!\E![100m,BRIGHTBLACK=!\E![90m,BRIGHTRED=!\E![91m,BRIGHTBLUE=!\E![94m,BRIGHTMAGENTA=!\E![95m"
 set "%el:,=" && set "%"
 echo !BGBLACK!!BRIGHTBLUE!
