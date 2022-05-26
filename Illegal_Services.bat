fab9077ce8297ef80bd40bc11d520fae86d97a43
Illegal-Services <BUZZARDGTA@protonmail.com>
2022-05-26 15:49:43 +0200

    v6.1.5.6 - 26/05/2022

--- Illegal_Services.bat
+++ Illegal_Services.bat
@@ -8,8 +8,8 @@ REM  Copyrights: Copyright (C) 2022 IB_U_Z_Z_A_R_Dl
 REM  Trademarks: Copyright (C) 2022 IB_U_Z_Z_A_R_Dl
 REM  Originalname: Illegal_Services.exe
 REM  Comments: Illegal Services
-REM  Productversion:  6. 1. 5. 5
-REM  Fileversion:  6. 1. 5. 5
+REM  Productversion:  6. 1. 5. 6
+REM  Fileversion:  6. 1. 5. 6
 REM  Internalname: Illegal_Services.exe
 REM  Appicon: Ressources\Icons\icon.ico
 REM  AdministratorManifest: Yes
@@ -197,7 +197,7 @@ for /f %%A in ('2^>nul dir "!TMPF!\????????.bat" /a:-d /o:-d /b ^| findstr /rxc:
 :LAUNCHER
 if defined VERSION set OLD_VERSION=!VERSION!
 if defined lastversion set OLD_LASTVERSION=!lastversion!
-set VERSION=v6.1.5.5 - 18/05/2022
+set VERSION=v6.1.5.6 - 26/05/2022
 set "el=UNDERLINE=!\E![04m,UNDERLINEOFF=!\E![24m,BLACK=!\E![30m,RED=!\E![31m,GREEN=!\E![32m,YELLOW=!\E![33m,BLUE=!\E![34m,MAGENTA=!\E![35m,CYAN=!\E![36m,WHITE=!\E![37m,BGBLACK=!\E![40m,BGYELLOW=!\E![43m,BGWHITE=!\E![47m,BGBRIGHTBLACK=!\E![100m,BRIGHTBLACK=!\E![90m,BRIGHTRED=!\E![91m,BRIGHTBLUE=!\E![94m,BRIGHTMAGENTA=!\E![95m"
 set "%el:,=" && set "%"
 echo !BGBLACK!!BRIGHTBLUE!
@@ -2471,22 +2471,24 @@ for /f "tokens=3" %%A in ('!bookmarks_parser.exe! -l -e "!IS_OUTPUT_DIRECTORY!\I
                     if "%%F"=="" (
                         if "%%D"=="0" (
                             if not "%%E"=="200" (
-                                if not "%%E"=="403" (
-                                    if "%%E"=="503" (
-                                        call :PROTECTED_WEBSITE_DETECTION url_src && (
+                                if not "%%E"=="429" (
+                                    if not "%%E"=="403" (
+                                        if "%%E"=="503" (
+                                            call :PROTECTED_WEBSITE_DETECTION url_src && (
+                                                set /a results+=1
+                                                echo !RED!%o2%: !YELLOW!!url_src! !RED!!o3! ^^!
+                                                curl.exe -fkLs "https://isitup.org/%%B" | >nul find /i "Oh no %%B" && (
+                                                    set /a results+=1
+                                                    echo !RED!%o2%: !YELLOW!!url_src! !RED!!o4! ^^!
+                                                )
+                                            )
+                                        ) else (
                                             set /a results+=1
                                             echo !RED!%o2%: !YELLOW!!url_src! !RED!!o3! ^^!
                                             curl.exe -fkLs "https://isitup.org/%%B" | >nul find /i "Oh no %%B" && (
                                                 set /a results+=1
                                                 echo !RED!%o2%: !YELLOW!!url_src! !RED!!o4! ^^!
                                             )
-                                        )
-                                    ) else (
-                                        set /a results+=1
-                                        echo !RED!%o2%: !YELLOW!!url_src! !RED!!o3! ^^!
-                                        curl.exe -fkLs "https://isitup.org/%%B" | >nul find /i "Oh no %%B" && (
-                                            set /a results+=1
-                                            echo !RED!%o2%: !YELLOW!!url_src! !RED!!o4! ^^!
                                         )
                                     )
                                 )
