b685038faa43156844eeedbdf89d3e88e4ee4f8d
Illegal-Services <BUZZARDGTA@protonmail.com>
2022-06-01 01:48:27 +0200

    v6.1.6.3 - 01/06/2022

--- Illegal_Services.bat
+++ Illegal_Services.bat
@@ -8,8 +8,8 @@ REM  Copyrights: Copyright (C) 2022 IB_U_Z_Z_A_R_Dl
 REM  Trademarks: Copyright (C) 2022 IB_U_Z_Z_A_R_Dl
 REM  Originalname: Illegal_Services.exe
 REM  Comments: Illegal Services
-REM  Productversion:  6. 1. 6. 2
-REM  Fileversion:  6. 1. 6. 2
+REM  Productversion:  6. 1. 6. 3
+REM  Fileversion:  6. 1. 6. 3
 REM  Internalname: Illegal_Services.exe
 REM  Appicon: Ressources\Icons\icon.ico
 REM  AdministratorManifest: Yes
@@ -197,7 +197,7 @@ for /f %%A in ('2^>nul dir "!TMPF!\????????.bat" /a:-d /o:-d /b ^| findstr /rxc:
 :LAUNCHER
 if defined VERSION set OLD_VERSION=!VERSION!
 if defined lastversion set OLD_LASTVERSION=!lastversion!
-set VERSION=v6.1.6.2 - 01/06/2022
+set VERSION=v6.1.6.3 - 01/06/2022
 set "el=UNDERLINE=!\E![04m,UNDERLINEOFF=!\E![24m,BLACK=!\E![30m,RED=!\E![31m,GREEN=!\E![32m,YELLOW=!\E![33m,BLUE=!\E![34m,MAGENTA=!\E![35m,CYAN=!\E![36m,WHITE=!\E![37m,BGBLACK=!\E![40m,BGYELLOW=!\E![43m,BGWHITE=!\E![47m,BGBRIGHTBLACK=!\E![100m,BRIGHTBLACK=!\E![90m,BRIGHTRED=!\E![91m,BRIGHTBLUE=!\E![94m,BRIGHTMAGENTA=!\E![95m"
 set "%el:,=" && set "%"
 echo !BGBLACK!!BRIGHTBLUE!
@@ -4922,7 +4922,7 @@ call :CHECK_FILE_SIGNATURE "!IS_OUTPUT_DIRECTORY!\IS.bookmarks.html" 35 IS_BOOKM
         call :IS_BOOKMARKS_COMPARE_NEW_DATE && (
             exit /b 0
         )
-        for /f "delims=" %%A in ('curl -fks "!git_raw_version!/sha1_IS.bookmarks.html.dat"') do (
+        for /f "delims=" %%A in ('curl -fkLs "!git_raw_version!/sha1_IS.bookmarks.html.dat"') do (
             call :GET_FILE_HASH_SHA1 "!IS_OUTPUT_DIRECTORY!\IS.bookmarks.html" && (
                 if "%%A"=="!file_hash!" (
                     exit /b 0
