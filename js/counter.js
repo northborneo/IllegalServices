6238af2ab4ae636597a3403fca84006f70e11f55
unknown <BUZZARDGTA@protonmail.com>
2022-11-20 12:13:58 +0100

    Update IS.bookmarks.html

--- js/counter.js
+++ js/counter.js
@@ -1,6 +1,6 @@
 document.write(`
     <div class="counter">
-        Updated: 13/11/2022&nbsp;&nbsp;|&nbsp;&nbsp;4657 links indexed.
+        Updated: 20/11/2022&nbsp;&nbsp;|&nbsp;&nbsp;5244 links indexed.
     </div>
     <br>
 `);
