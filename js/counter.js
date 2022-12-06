49ecb1396884afca1ed002ca51b53d61249cefe2
IB_U_Z_Z_A_R_Dl <BUZZARDGTA@protonmail.com>
2022-12-06 16:45:08 +0100

    Update IS.bookmarks.html

--- js/counter.js
+++ js/counter.js
@@ -1,6 +1,6 @@
 document.write(`
     <div class="counter">
-        Updated: 27/11/2022&nbsp;&nbsp;|&nbsp;&nbsp;5547 links indexed.
+        Updated: 06/12/2022&nbsp;&nbsp;|&nbsp;&nbsp;5546 links indexed.
     </div>
     <br>
 `);
