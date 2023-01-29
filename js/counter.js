8d93822cfedb7248c8350a836c0306494b8d8635
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2023-01-29 13:37:40 +0100

    Update IS.bookmarks.html

--- js/counter.js
+++ js/counter.js
@@ -1,6 +1,6 @@
 document.write(`
     <div class="counter">
-        Updated: 29/01/2023&nbsp;&nbsp;|&nbsp;&nbsp;5831 links indexed.
+        Updated: 29/01/2023&nbsp;&nbsp;|&nbsp;&nbsp;5859 links indexed.
     </div>
     <br>
 `);
